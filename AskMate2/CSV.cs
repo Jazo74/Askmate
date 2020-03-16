7632ef515a2b57395c9bf3220d12984161b21c12
Erik-Aron-Szabo <erik.aron.szabo@gmail.com>
2020-03-16 13:16:34 +0100

    Proba1

---
+++ AskMate2/CSV.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskMate2
+{
+    public class CSV //ide a csv-s dolgok jojjenek
+    {
+    }
+}
