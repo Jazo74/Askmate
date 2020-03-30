a500aedebf126f61f75e36b29492a420f21834ac
Erik-Aron-Szabo <erik.aron.szabo@gmail.com>
2020-03-30 14:32:18 +0200

    Environment

--- AskMate2/Program.cs
+++ AskMate2/Program.cs
@@ -12,7 +12,13 @@ namespace AskMate2
 {
     public class Program
     {
-        
+
+        private static readonly string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+        private static readonly string dbUser = Environment.GetEnvironmentVariable("DB_USER");
+        private static readonly string dbPass = Environment.GetEnvironmentVariable("DB_PASS");
+        private static readonly string dbName = Environment.GetEnvironmentVariable("DB_NAME");
+        public static readonly string ConnectionString = $"Host={dbHost};Username={dbUser};Password={dbPass};Database={dbName}";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
