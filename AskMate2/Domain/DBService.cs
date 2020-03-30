e85b9a2855a32d5b4e92520ae60994cfe39f810d
Erik-Aron-Szabo <erik.aron.szabo@gmail.com>
2020-03-30 13:58:47 +0200

    DBService added

---
+++ AskMate2/Domain/DBService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskMate2.Domain
+{
+    public class DBService
+    {
+
+
+    }
+}
